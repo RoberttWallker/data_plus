7353843e275bb7d3ca59129ba2387cf078dc4ff2
RoberttWallker <robertwalker.rodrigues@gmail.com>
2024-12-08 22:16:44 -0300

    Adições em menu main.

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ def main():
     while True:
         resposta = input('''
     Escolha uma das opções abaixo:
-        1 - Iniciar criação do banco de dados completo.
-        2 - Gravar configuração de banco de dados.
-        3 - Gravar configuração de nova API.                                                                                
-        >>>'''
-        )
+    1 - Iniciar criação do banco de dados completo.
+    2 - Gravar configuração de banco de dados.
+    3 - Gravar configuração de nova API. 
+    Q - Sair                                                                               
+    >>>'''
+).upper()
         if resposta == "1":
-
             init_creation_db()
 
             init_creation_requests()
@@ -25,7 +25,23 @@ def main():
             time.sleep(1)
             total_data_requests()
 
+            time.sleep(1)
             total_inserter()
+        
+        elif resposta == "2":
+            init_creation_db()
+
+        elif resposta == "3":
+            init_creation_requests()
+
+        elif resposta == "Q":
+            print("Encerrando...")
+            time.sleep(1)
+            exit()
+
+        else:
+            print("Opção incorreta, tente novamente.")
+
 
 if __name__ == "__main__":
     main()
